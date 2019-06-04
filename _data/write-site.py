23770c203b065582517302025af7b88744cef219
Katy Huff <katyhuff@gmail.com>
2019-06-04 13:44:50 -0400

    write-site update

---
+++ _data/write-site.py
@@ -0,0 +1,70 @@
+#!/usr/bi.env python
+import csv
+import jinja2
+import numpy as np
+import os
+import sys
+
+def get_id(num):
+    return "{0:0=2d}".format(num)
+
+def get_item(data, name):
+    item = data[name].decode('utf-8')
+    print(type(item), item)
+    return item
+
+def file_name(num, title, presenter):
+    print(presenter)
+    lastname= presenter.lower().split(' ', 1)[1]
+    firstword = title.lower().split(' ', 1)[0]
+    file_name = get_id(num) + '-' + lastname + '-' + firstword + 'pdf'
+    return file_name
+
+
+# print(abstracts)
+
+def print_program(data):
+    num = 0
+    for abstract in data:
+        print(num)
+        title = get_item(abstract, 'title')
+        presenter = get_item(abstract, 'presenter')
+        program_body =template.render(
+            date=get_item(abstract, 'date'),
+            session=get_item(abstract, 'session'),
+            sessiontype=get_item(abstract, 'sessiontype'),
+            time=get_item(abstract, 'time'),
+            presenter=get_item(abstract, 'presenter'),
+            coauthors=get_item(abstract, 'coauthors'),
+            chair=get_item(abstract, 'chair'),
+            title=get_item(abstract, 'title'),
+            abstract_text=get_item(abstract, 'abstract_text'),
+            filename=file_name(num, title, presenter))
+        num += 1
+    print(program_body)
+    return program_body
+
+
+# Main activity·
+# Get commandline args
+if len(sys.argv) < 3:
+    print('Usage: ./write-site.py abstracts.csv program.yml.in')
+
+# identify data path
+data_path = sys.argv[1]
+
+# open the file
+with open(sys.argv[2], 'r') as fp:
+    program_template = fp.read()
+template = jinja2.Template(program_template)
+
+# column names
+cols = ('date', 'time', 'duration', 'sessiontype', 'session', 'chair', 'presenter',
+        'coauthors', 'title', 'abstract_text')
+
+# get data
+abstracts = np.genfromtxt(data_path, delimiter='\t', dtype=None, names=cols,
+                          skip_header=1)
+
+# print
+print_program(abstracts)
